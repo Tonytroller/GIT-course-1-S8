5e83cca6940a36d4deb3cde335672545696d96fa
EmericBsn <116282280+EmericBsn@users.noreply.github.com>
2023-03-10 11:11:05 +0100

    Modification question2

--- robot.py
+++ robot.py
@@ -1,6 +1,13 @@
+from datetime import datetime
+
 class Robot:
     def __init__(self, name):
         self.name = name
 
     def say_hello(self):
         print(f"hello, my name is {self.name}")
+
+    @staticmethod
+    def give_date_and_time(self):
+        current_datetime = datetime.now()
+        print(current_datetime)
