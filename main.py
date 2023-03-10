e42204f5e8b4b0b45d534e0d96ff983adbfc4e19
EmericBsn <116282280+EmericBsn@users.noreply.github.com>
2023-03-10 11:34:21 +0100

    Forgot to add give date and time in main

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from robot import Robot
 def start():
     robot = Robot(robot="my_robot_name")
     robot.say_hello()
-
+    robot.give_date_and_time(Robot)
 
 if __name__ == '__main__':
     start()
